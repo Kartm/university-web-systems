c148719498696f99334d1447e8b32ac9e2d66762
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-11-30 17:19:43 +0100

    Move game code to a class

--- lab4/game.js
+++ lab4/game.js
@@ -1,101 +1,110 @@
-const squares = [
-  { cssBackgroundColor: "magenta", key: "m" },
-  { cssBackgroundColor: "yellow", key: "y" },
-  { cssBackgroundColor: "green", key: "g" },
-  { cssBackgroundColor: "orange", key: "o" },
-  { cssBackgroundColor: "pink", key: "p" },
-];
+class Game {
+  squares = [
+    { cssBackgroundColor: "magenta", key: "m" },
+    { cssBackgroundColor: "yellow", key: "y" },
+    { cssBackgroundColor: "green", key: "g" },
+    { cssBackgroundColor: "orange", key: "o" },
+    { cssBackgroundColor: "pink", key: "p" },
+  ];
 
-let gameBoxElement;
-let scoreElement;
+  constructor() {
+    this.gameBoxElement = gameBoxElement;
+    this.scoreElement = scoreElement;
 
-const gameState = {
-  active: false,
-  points: 0,
-};
+    this.state = {
+      active: false,
+      points: 0,
+    };
 
-const addAndDisplayPoints = (newPoints) => {
-  gameState.points += newPoints;
-  scoreElement.innerHTML = `Points: ${gameState.points}`;
-};
+    this.addAndDisplayPoints(0);
+  }
 
-const onSquareClicked = (data, target) => {
-  console.log(data, target);
+  get isActive() {
+    return this.state.active;
+  }
 
-  target.style.display = "none";
-  addAndDisplayPoints(1);
-};
-
-const initializeGame = () => {
-  gameState.points = 0;
-
-  const getSquareSize = () => {
+  get squareSize() {
     const smaller = Math.min(
-      gameBoxElement.getBoundingClientRect().width,
-      gameBoxElement.getBoundingClientRect().height
+      this.gameBoxElement.getBoundingClientRect().width,
+      this.gameBoxElement.getBoundingClientRect().height
     );
 
     return smaller / 5;
-  };
-
-  const squareSize = getSquareSize();
-
-  const nonOverlappingPositions = [];
-
-  while (nonOverlappingPositions.length < 5) {
-    const { width, height } = gameBoxElement.getBoundingClientRect();
-    var x = Math.floor(Math.random() * (width - squareSize));
-    var y = Math.floor(Math.random() * (height - squareSize));
-
-    if (
-      nonOverlappingPositions.every((p) => {
-        // If one rectangle is on left side of other
-        if (x + squareSize <= p.x || p.x + squareSize <= x) {
-          return true;
-        }
-
-        // If one rectangle is above other
-        if (y > p.y + squareSize || p.y > y + squareSize) {
-          return true;
-        }
-
-        return false;
-      })
-    ) {
-      nonOverlappingPositions.push({ x, y });
-    }
   }
 
-  gameBoxElement.innerHTML = "";
+  begin() {
+    this.state.active = true;
 
-  squares.forEach((s, i) => {
-    const { x, y } = nonOverlappingPositions[i];
+    const nonOverlappingPositions = [];
 
-    const box = document.createElement("div");
-    box.style.backgroundColor = s.cssBackgroundColor;
-    box.style.width = `${squareSize}px`;
-    box.style.height = `${squareSize}px`;
-    box.style.position = "absolute";
-    box.style.bottom = `${y}px`;
-    box.style.left = `${x}px`;
-    box.style.cursor = "pointer";
+    while (nonOverlappingPositions.length < 5) {
+      const { width, height } = this.gameBoxElement.getBoundingClientRect();
+      var x = Math.floor(Math.random() * (width - this.squareSize));
+      var y = Math.floor(Math.random() * (height - this.squareSize));
 
-    box.addEventListener("click", (e) => onSquareClicked({ s }, e.target));
+      if (
+        nonOverlappingPositions.every((p) => {
+          // If one rectangle is on left side of other
+          if (x + this.squareSize <= p.x || p.x + this.squareSize <= x) {
+            return true;
+          }
 
-    gameBoxElement.appendChild(box);
-  });
-};
+          // If one rectangle is above other
+          if (y > p.y + this.squareSize || p.y > y + this.squareSize) {
+            return true;
+          }
+
+          return false;
+        })
+      ) {
+        nonOverlappingPositions.push({ x, y });
+      }
+    }
+
+    this.gameBoxElement.innerHTML = "";
+
+    this.squares.forEach((s, i) => {
+      const { x, y } = nonOverlappingPositions[i];
+
+      const box = document.createElement("div");
+      box.style.backgroundColor = s.cssBackgroundColor;
+      box.style.width = `${this.squareSize}px`;
+      box.style.height = `${this.squareSize}px`;
+      box.style.position = "absolute";
+      box.style.bottom = `${y}px`;
+      box.style.left = `${x}px`;
+      box.style.cursor = "pointer";
+
+      box.addEventListener("click", (e) =>
+        this.onSquareClicked({ s }, e.target)
+      );
+
+      this.gameBoxElement.appendChild(box);
+    });
+  }
+
+  addAndDisplayPoints(newPoints) {
+    this.state.points += newPoints;
+    scoreElement.innerHTML = `Points: ${this.state.points}`;
+  }
+
+  onSquareClicked(data, target) {
+    console.log(data, target);
+
+    target.style.display = "none";
+    this.addAndDisplayPoints(1);
+  }
+}
 
 window.addEventListener("DOMContentLoaded", function () {
   gameBoxElement = document.getElementById("game-box");
   scoreElement = document.getElementById("score");
-  addAndDisplayPoints(0);
+
+  const game = new Game(gameBoxElement, scoreElement);
 
   gameBoxElement.addEventListener("mouseover", () => {
-    if (!gameState.active && confirm("Wanna play?")) {
-      gameState.active = true;
-
-      initializeGame();
+    if (!game.isActive && confirm("Wanna play?")) {
+      game.begin();
     }
   });
 });
