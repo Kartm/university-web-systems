54c3484da8c795822c0478d5e383fe25a2da4b62
CaptainBoii <77623616+CaptainBoii@users.noreply.github.com>
2021-12-01 22:17:48 +0100

    I hate javascript

--- lab4/game.js
+++ lab4/game.js
@@ -10,10 +10,21 @@ class Game {
   constructor() {
     this.gameBoxElement = gameBoxElement;
     this.scoreElement = scoreElement;
+	this.color_field = color_field;
+	window.addEventListener("keyup", function (e) {
+	if (check_input && e.key != "Control") {
+		let inputVal = document.getElementById("input_color").value;
+		let temp = colors[color_field];
+		if (inputVal != "" && inputVal === temp.charAt(0)) {
+			scoreElement.innerHTML = `Points: ${++result}`;
+		}
+		document.getElementById("input_color").value = "";
+		check_input=false;
+	}
+  });
 
     this.state = {
       active: false,
-      points: 0,
     };
 
     this.addAndDisplayPoints(0);
@@ -81,48 +92,81 @@ class Game {
       cursor: "pointer",
     };
   }
-
+  
   begin() {
     this.state.active = true;
 
     const nonOverlappingSquares = this.generateNonOverlappingSquares();
 
     this.squares.forEach((s, i) => {
+		console.log(s);
       const box = document.createElement("div");
 
       const coordinates = nonOverlappingSquares[i];
       Object.assign(box.style, this.generateSquareCss(s, coordinates));
-
+		if (s["cssBackgroundColor"] != "yellow") {
       box.addEventListener("click", (e) =>
         this.onSquareClicked({ s }, e.target)
-      );
-
+      );}
+	  else {
+		box.addEventListener("click", (e) =>
+        this.onYellowClicked({ s }, e.target, e.ctrlKey)
+      );}
       this.gameBoxElement.appendChild(box);
-    });
+	});
   }
+
 
   addAndDisplayPoints(newPoints) {
-    this.state.points += newPoints;
-    scoreElement.innerHTML = `Points: ${this.state.points}`;
+    result += newPoints;
+    scoreElement.innerHTML = `Points: ${result}`;
+	if (result === 5) {
+		check_input=true;
+		document.getElementById("input_color").value = "";
+		setTimeout(function(){
+		check_input = false;
+		var res = "You've got ";
+		res += result;
+		res += " points!";
+		alert(res);}, 5000);
+	}
   }
+  
 
+  onYellowClicked(data, target, key) {
+	 console.log(data, target);
+	if(key) {
+		target.style.display = "none";
+		this.addAndDisplayPoints(1);
+	}
+  }
+	
   onSquareClicked(data, target) {
     console.log(data, target);
 
     target.style.display = "none";
     this.addAndDisplayPoints(1);
   }
+  
 }
+
+var check_input = false;
+colors = ["pink", "yellow", "orange", "violet", "red", "green", "blue"];
+var result = 0;
 
 window.addEventListener("DOMContentLoaded", function () {
   gameBoxElement = document.getElementById("game-box");
   scoreElement = document.getElementById("score");
-
-  const game = new Game(gameBoxElement, scoreElement);
-
+  color_field = Math.floor(Math.random() * colors.length);
+	
+  const game = new Game(gameBoxElement, scoreElement, color_field);
+    let this_color = colors[color_field];
+  inputVal = document.getElementById("input_color");
+  inputVal.style.backgroundColor = this_color;
   gameBoxElement.addEventListener("mouseover", () => {
     if (!game.isActive && confirm("Wanna play?")) {
       game.begin();
     }
   });
 });
+
