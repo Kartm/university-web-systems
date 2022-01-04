ea11ac18d5e71ec23c525f8192304f97e19e9cc0
Lukasz Blachnicki <lblachnicki@gmail.com>
2022-01-04 15:00:39 +0100

    Finish form

--- jsf1/mavenproject2/src/main/java/com/mycompany/mavenproject2/ValidationBean.java
+++ jsf1/mavenproject2/src/main/java/com/mycompany/mavenproject2/ValidationBean.java
@@ -9,51 +9,64 @@ public class ValidationBean implements Serializable
    private String name;
    private String email;
    private String phone;
+   private int age;
+   private boolean isStudent;
    
-   // return the name String
    public String getName()
    {
       return name;
-   } // end method getName
+   }
 
-   // set the name String
    public void setName( String name )
    {
       this.name = name;
-   } // end method setName
+   }
 
-   // return the email String
    public String getEmail()
    {
       return email;
-   } // end method getEmail
+   }
 
-   // set the email String
    public void setEmail( String email )
    {
       this.email = email;
-   } // end method setEmail
+   }
 
-   // return the phone String
    public String getPhone()
    {
       return phone;
-   } // end method getPhone
+   }
 
-   // set the phone String
    public void setPhone( String phone )
    {
       this.phone = phone;
-   } // end method setPhone
+   }
 
-   // returns result for rendering on the client
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean getIsStudent() {
+        return this.isStudent;
+    }
+
+    public void setIsStudent(boolean isStudent) {
+        this.isStudent = isStudent;
+    }
+
    public String getResult()
    {
       if ( name != null && email != null && phone != null )
-         return "<p style=\"background-color:yellow;width:200px;" +
-            "padding:5px\">Name: " + getName() + "<br/>E-Mail: " +
+         return "<p style=\"background-color:grey;width:200px;" +
+            "padding:10px\"> " + (getIsStudent() ? "STUDENT :)" : "NOT STUDENT :(") + 
+            " <br/>Age: " + getAge() +
+            " <br/>Name: " + getName() + "<br/>E-Mail: " +
             getEmail() + "<br/>Phone: " + getPhone() + "</p>";
       else
-         return ""; // request has not yet been made
-   } // end method getResult
-} // end class ValidationBean
+         return "";
+   }
+}
